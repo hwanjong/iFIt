83f09b024ba2447e323e56ee8a7c8cd91bb36464
Nam-Byung-Woo <namjjang6@gmail.com>
2016-03-28 03:30:57 +0900

    슬라이더 작업전 중간 저장

--- iFit/WebContent/js/moreInfoPage.js
+++ iFit/WebContent/js/moreInfoPage.js
@@ -1,6 +1,38 @@
 $(document).ready(function(e){
-	$(".carousel-inner>.item").on("swipeleft",function(e){ $("#myCarousel1").carousel("next");});
-	$(".carousel-inner>.item").on("swiperight",function(e){ $("#myCarousel1").carousel("prev");});
+	var jssor_1_SlideshowTransitions = [{$Duration:1200,$Opacity:2}];
+	                                  
+	var jssor_1_options = {
+		$AutoPlay: true,
+		$SlideshowOptions: {
+			$Class: $JssorSlideshowRunner$,
+			$Transitions: jssor_1_SlideshowTransitions,
+			$TransitionsOrder: 1
+		},
+		$ArrowNavigatorOptions: {
+			$Class: $JssorArrowNavigator$
+		},
+		$BulletNavigatorOptions: {
+			$Class: $JssorBulletNavigator$
+		}
+	};
+	
+	var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+	
+	//responsive code begin
+	//you can remove responsive code if you don't want the slider scales while window resizing
+	function ScaleSlider() {
+		var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+		if (refSize) {
+			refSize = Math.min(refSize, 600);
+			jssor_1_slider.$ScaleWidth(refSize);
+		}else {
+			window.setTimeout(ScaleSlider, 30);
+		}
+	}
+	ScaleSlider();
+	$(window).bind("load", ScaleSlider);
+	$(window).bind("resize", ScaleSlider);
+	$(window).bind("orientationchange", ScaleSlider);
 });
 function moreInfoClosetClickHandler(e){
 	alert("옷장 이동");
