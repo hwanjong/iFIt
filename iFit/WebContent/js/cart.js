e0d32c5cd2c57db296621349e393d714c219f77f
Nam-Byung-Woo <namjjang6@gmail.com>
2016-03-30 05:49:49 +0900

    장바구니 완료

--- iFit/WebContent/js/cart.js
+++ iFit/WebContent/js/cart.js
@@ -35,6 +35,17 @@ function removeCart(productId){
 	$("#cartPage").find("#"+productId).remove();
 	reCal();
 }
+
+function itemPriceSum(){
+	var sum = 0;
+	$(".item_price_list").each(function(){
+		if($(this).css("display") != "none"){
+			sum += parseInt(($(this).children("p:last-child").html()).replace(/[^0-9]/g,""));
+		}
+	});
+	$("#item_price_sum").html("￦"+sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+	
+}
 $(document).ready(function() {
 //	reCal();
 	
@@ -43,17 +54,64 @@ $(document).ready(function() {
 			var amount = $(this).parents().children(".cartAmount");
 			var count = $(amount).text();
 			$(amount).text(parseInt(count)+1);
+			var itemObj = $("#item_price_"+$(this).parents(".cart_item").attr("data-item-idx")+" p:first-child");
+			if(parseInt(count)+1>1){
+				itemObj.children("span").show();
+			}else{
+				itemObj.children("span").hide();
+			}
+			itemObj.children("span:last-child").html(parseInt(count)+1);
+			var itemPrice = $(this).siblings(".itemPrice").html();
+			itemPrice = itemPrice.replace(/[^0-9]/g,"");
+			var calPriceString = "￦"+(itemPrice*(parseInt(count)+1)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+			itemObj.siblings().html(calPriceString);
+			itemPriceSum();
 		}
-		alert($(this).parents("cart_item").attr("data-item-idx"));
+
 	})
 	
 	$(".numDown").click(function(){
 		if($(this).parents(".cart_item").hasClass("itemOn") === true) {
 			var amount = $(this).parents().children(".cartAmount");
 			var count = $(amount).text();
+			if(parseInt(count)>1)	$(amount).text(parseInt(count)-1);
+			
+			var itemObj = $("#item_price_"+$(this).parents(".cart_item").attr("data-item-idx")+" p:first-child");
+			if(parseInt(count)-1>1){
+				itemObj.children("span").show();
+			}else{
+				itemObj.children("span").hide();
+			}
+			
+			if(parseInt(count)-1>0){
+				itemObj.children("span:last-child").html(parseInt(count)-1);
+				var itemPrice = $(this).siblings(".itemPrice").html();
+				itemPrice = itemPrice.replace(/[^0-9]/g,"");
+				var calPriceString = "￦"+(itemPrice*(parseInt(count)-1)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+				itemObj.siblings().html(calPriceString);
+				itemPriceSum();
+			}
+		}
+	})
+	
+	
+	// 웨딩쪽 시연때문에 임시로
+	$(".numUp_w").click(function(){
+		if($(this).parents(".cart_item").hasClass("itemOn") === true) {
+			var amount = $(this).parents().children(".cartAmount");
+			var count = $(amount).text();
+			$(amount).text(parseInt(count)+1);
+		}
+	})
+	
+	$(".numDown_w").click(function(){
+		if($(this).parents(".cart_item").hasClass("itemOn") === true) {
+			var amount = $(this).parents().children(".cartAmount");
+			var count = $(amount).text();
 			if(count!=0)	$(amount).text(parseInt(count)-1);
 		}
 	})
+
 });
 
 
@@ -72,9 +130,13 @@ $(function(){
 		if($(this).parents(".cart_item").hasClass("itemOn") === true) {
 			$(this).parents(".cart_item").removeClass("itemOn");
 			$(this).parents(".cart_item").addClass("itemOff");
-		}else if($(this).parents(".cart_item").hasClass("itemOff") === true === true) {
+			$("#item_price_"+$(this).parents(".cart_item").attr("data-item-idx")).hide();
+			itemPriceSum();
+		}else if($(this).parents(".cart_item").hasClass("itemOff") === true) {
 			$(this).parents(".cart_item").removeClass("itemOff");
 			$(this).parents(".cart_item").addClass("itemOn");
+			$("#item_price_"+$(this).parents(".cart_item").attr("data-item-idx")).show();
+			itemPriceSum();
 		}
 	});
 	$(".itemDeleteBtn").click(function(){
