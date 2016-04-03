31d94345c3fe7da6da155fb4e66dbc281ca34b83
Nam-Byung-Woo <namjjang6@gmail.com>
2016-04-04 03:43:38 +0900

    일부제외하고 거의 완료된듯?(마이페이지 완료)

--- iFit/WebContent/js/mypage.js
+++ iFit/WebContent/js/mypage.js
@@ -34,15 +34,48 @@ $(document).on("click","#logoutBtn",function(e){
 function idSearchPopupHandlerRegist(){
 	$(document).on("click","#idSearch",function(e){
 		$("#idSearchPopup").popup("open");
+		$("#idSearchPopup").bind({
+			popupafterclose: function(event, ui) {
+				$("#idSearchTitle").val("");
+			}
+		});
+	});
+}
+function pwSearchPopupHandlerRegist(){
+	$(document).on("click","#pwSearch",function(e){
+		$("#pwSearchPopup").popup("open");
+		$("#pwSearchPopup").bind({
+			popupafterclose: function(event, ui) {
+				$("#pwSearchTitle").val("");
+			}
+		});
+	});
+}
+function updateUserInfoPopupHandlerRegist(){
+	$(document).on("click","#updateUserInfo",function(e){
+		$("#updateUserInfoPopup").popup("open");
+		$("#updateUserInfoPopup").bind({
+			popupafterclose: function(event, ui) {
+				$("#updateUserInfoTitle").val("");
+			}
+		});
 	});
 }
 
-$(document).on("click","#cancelBtn",function(e){
-	$("#registPopup").popup("close");
+$(document).on("click","#idSearchConfirm",function(e){
+	$("#idSearchPopup").popup("close");
 });
 
-$(document).on("click","#writeBtn",function(e){
-	$("#registPopup").popup("close");
+$(document).on("click","#pwSearchConfirm",function(e){
+	$("#pwSearchPopup").popup("close");
+});
+
+$(document).on("click","#updateUserInfoConfirm",function(e){
+	$("#updateUserInfoPopup").popup("close");
+//	saveScroll();
+	window.open("updateUserInfo.html", '_self'); 
 });
 
 idSearchPopupHandlerRegist();
+pwSearchPopupHandlerRegist();
+updateUserInfoPopupHandlerRegist();
